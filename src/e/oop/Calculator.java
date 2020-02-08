e1abadad5d7be526ffaa36cb33ca7ceedd39d963
Jongwoo <dlwhddn2012@gmail.com>
2020-02-08 13:47:38 +0900

    first commit

---
+++ src/e/oop/Calculator.java
@@ -0,0 +1,27 @@
+package e.oop;
+
+public class Calculator { 
+		//계산기 클래스
+	
+	
+	int plus(int a,int b){
+			return a + b;
+			
+	}
+	
+	double minus(double a, double b){
+		return a - b;
+	}
+	
+	double multiply(double a, double b){
+		 return a* b;
+	}
+	
+	double division(double a, double b){
+		return a / b;
+	}
+	
+	double remainder(double a, double b){
+		return a % b;
+	}
+}
